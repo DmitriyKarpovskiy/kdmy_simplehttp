44f366db0520b9b7651f7de6738f596d4347d5a3
Дмитрий Карповский <dmkarpovskiy@gmail.com>
2019-02-11 15:31:42 +0200

    initial commit

--- kdmy_http/Program.cs
+++ kdmy_http/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,25 @@ namespace kdmy_http
     {
         static void Main(string[] args)
         {
+            using (var client = new TcpClient("www.google.com", 80))
+            {
+                using (var stream = client.GetStream())
+                using (var writer = new StreamWriter(stream))
+                using (var reader = new StreamReader(stream))
+                {
+                    writer.AutoFlush = true;
+                    // Send request headers
+                    writer.WriteLine("GET / HTTP/1.0\n\n");
+                    writer.WriteLine("Host: www.google.com:80");
+                    writer.WriteLine("Connection: close");
+                    writer.WriteLine();
+                    writer.WriteLine();
+
+                    // Read the response from server
+                    Console.WriteLine(reader.ReadToEnd());
+                    Console.ReadKey();
+                }
+            }
         }
     }
 }
